8b2c02e6df28fce22da69d61e11eeb393f4fe702
smukher6-stack <smukher6@pratt.edu>
2026-02-19 14:35:58 -0500

    started walking script

---
+++ walking sim/Assets/playerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 7f;
+    public float jumpHeight = 5f;
+
+    public Transform cameraTransform;
+    public float lookSensitivity = 1f;
+
+    pri
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
