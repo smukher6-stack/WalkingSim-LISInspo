14fe8ba5ba46d1fd5a8dc6a292925d44b35ce1eb
smukher6-stack <smukher6@pratt.edu>
2026-02-26 14:19:01 -0500

    new scene stuff

--- walking sim/Assets/UIListen.cs
+++ walking sim/Assets/UIListen.cs
@@ -1,16 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 public class UIListen : MonoBehaviour
 {
+
+    public TextMeshProUGUI statusText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void UpdateText()
     {
-        
+
+        statusText.text = "Button Pressed";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEnable()
     {
-        
+        buttonEvent.onButtonPressed += UpdateText;
+    }
+
+    public void OnDisable()
+    {
+        buttonEvent.onButtonPressed -= UpdateText;
     }
 }
