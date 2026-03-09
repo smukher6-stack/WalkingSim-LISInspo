87724aaea94fb1ba4c8d674161a66f0397716551
smukher6-stack <smukher6@pratt.edu>
2026-03-09 13:36:55 -0400

    i think im getting somewhewre

--- walking sim/Assets/scripting/PlayerItemCollector.cs
+++ walking sim/Assets/scripting/PlayerItemCollector.cs
@@ -13,7 +13,7 @@ public class PlayerItemCollector : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.CompareTag("puzzle piece"))
+        if (collision.CompareTag("puzzle piece") && Input.GetKeyDown(KeyCode.R))
         {
             slotScript item = collision.GetComponent<slotScript>();
             if (item != null)
