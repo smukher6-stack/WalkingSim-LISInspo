566a4fa38077fe1eba53b7238841fca2f7c3d55a
smukher6-stack <smukher6@pratt.edu>
2026-05-09 13:20:41 -0400

    iiiiiiiiiiiiiiiiiiiiiiiiiiii gaaaaaaaaaaave uuuuuuuuuuuuuuuuuuuuuuuuup

--- walking sim/Assets/scripting/inventorySystem.cs
+++ walking sim/Assets/scripting/inventorySystem.cs
@@ -8,16 +8,26 @@ public class inventorySystem : MonoBehaviour
     public GameObject diaryPages;
     public Image inventoryImage;
     public Button inventoryButton;
+    public puzzleScript script;
 
     
     void Awake()
     {
-        
+        inventoryBar = GetComponent<GameObject>();
+        inventoryItem = GetComponent<GameObject>();
+        diaryPages = GetComponent<GameObject>();
+        inventoryImage = GetComponent<Image>();
+        inventoryButton = GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+   public void InventoryTab()
+    {
+
     }
 }
--- walking sim/Assets/scripting/playerMovement.cs
+++ walking sim/Assets/scripting/playerMovement.cs
@@ -200,4 +200,6 @@ public class playerMovement : MonoBehaviour
         OnObjectDescripton?.Invoke(data);
     }
 
+    
+
 }
