6295ef49e1490e404955c04dfb0a26769f5e6c9f
smukher6-stack <smukher6@pratt.edu>
2026-05-09 13:03:15 -0400

    ok inventory system is go

---
+++ walking sim/Assets/scripting/inventorySystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class inventorySystem : MonoBehaviour
+{
+    public GameObject inventoryBar;
+    public GameObject inventoryItem;
+    public GameObject diaryPages;
+    public Image inventoryImage;
+    public Button inventoryButton;
+
+    
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
